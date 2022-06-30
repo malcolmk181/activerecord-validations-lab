4824199d716fb78e2b3406cae462a520f77329b3
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-30 07:49:04 -0400

    commit tree 131eacaa95d7a91bc4f5b3679bb758f2296ed6e1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,29 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :title_is_clickbait
+
+    private
+
+    def title_is_clickbait
+        clickbait = [
+            /Won't Believe/,
+            /Secret/,
+            /Top [0-9]+/,
+            /Guess/
+        ]
+
+        if title.nil? then
+            errors.add(:title, "must not be nil")
+            return
+        end
+
+        clickbait.each do |c|
+            if self.title.match(c) then return ; end
+        end
+
+        errors.add(:title, "must be clickbait")
+    end
 end
